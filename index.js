0f1318c0115e980858e7a29980d621acf52b2961
d061059 <Alexander.Hauck@sap.com>
2021-08-23 17:33:47 +0200

    test commit

--- index.js
+++ index.js
@@ -28,5 +28,6 @@ app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 
+//this is a test command
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
