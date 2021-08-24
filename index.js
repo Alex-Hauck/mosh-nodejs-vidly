fc7fa0b2a2321db6dc838ad965e118d89dd086c5
d061059 <Alexander.Hauck@sap.com>
2021-08-24 10:58:56 +0200

    - Add options to change to new connection behavior for mongo db

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ if (!config.get('jwtPrivateKey')) {
 }
 
 mongoose
-  .connect('mongodb://vidly:password@localhost/playground')
+  .connect('mongodb://vidly:password@localhost/playground', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(() => console.error('Could not connect to MongoDB...'));
 
